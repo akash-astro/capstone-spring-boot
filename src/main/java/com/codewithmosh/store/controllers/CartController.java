a303d477bf7f8320dc72baa30d3192fe05e282df
Akashmed <akashmed3@gmail.com>
2025-12-05 00:33:05 +0600

    capstone project, clearing carts

--- src/main/java/com/codewithmosh/store/controllers/CartController.java
+++ src/main/java/com/codewithmosh/store/controllers/CartController.java
@@ -111,4 +111,14 @@ public class CartController {
         cartRepository.save(cart);
         return ResponseEntity.noContent().build();
     }
+
+    @DeleteMapping("/{cartId}/items")
+    public ResponseEntity<Void> clearCart(@PathVariable UUID cartId){
+        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
+        if(cart == null) return ResponseEntity.notFound().build();
+
+        cart.clear();
+        cartRepository.save(cart);
+        return ResponseEntity.noContent().build();
+    }
 }
