e295df4b576f3ce939380e500893feb0468690ab
Akashmed <akashmed3@gmail.com>
2025-12-04 23:39:03 +0600

    capstone project, refactoring towards an object-oriented programming

--- src/main/java/com/codewithmosh/store/entities/Cart.java
+++ src/main/java/com/codewithmosh/store/entities/Cart.java
@@ -27,10 +27,31 @@ public class Cart {
     @OneToMany(mappedBy = "cart", cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
     private Set<CartItem> items = new LinkedHashSet<>();
 
-    public BigDecimal getTotalPrice(){
+    public BigDecimal getTotalPrice() {
         return items.stream()
                 .map(CartItem::getTotalPrice)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public CartItem getItem(Long productId) {
+        return items.stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst().orElse(null);
+    }
+
+    public CartItem addItem(Product product){
+        var cartItem = getItem(product.getId());
+        if (cartItem != null) {
+            cartItem.setQuantity(cartItem.getQuantity() + 1);
+        } else {
+            cartItem = new CartItem();
+            cartItem.setProduct(product);
+            cartItem.setQuantity(1);
+            cartItem.setCart(this);
+            items.add(cartItem);
+        }
+
+        return cartItem;
     }
 
 }
